9d82b8a8de074001cc1fdef8b1c4f08e4afb4c4d
Probo Krishnacahya <probo.krishnacahya@gmail.com>
2023-05-05 12:55:49 +0700

    Initial Commit

---
+++ HelloBaby/HelloBabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBabyApp.swift
+//  HelloBaby
+//
+//  Created by Probo Krishnacahya on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloBabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
